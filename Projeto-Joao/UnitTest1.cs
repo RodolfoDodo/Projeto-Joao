2abd890e9c4305eb0b4f551f0e711ba805074f56
Rodolfo Farley <farleyrodolfo@gmail.com>
2023-06-03 01:08:40 -0300

    first commit

---
+++ Projeto-Joao/UnitTest1.cs
@@ -0,0 +1,26 @@
+
+
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Projeto_Joao
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://hom.eadtech.net/App/Student/User/Account/Login");
+
+            Thread.Sleep(5000);
+
+            driver.Quit();
+        }
+    }
+}
